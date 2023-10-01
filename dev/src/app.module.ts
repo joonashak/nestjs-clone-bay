733e6b91a77f5118546b116a1c26f93c248fc14d
joonashak <joonashak@proton.me>
2023-10-01 10:30:28 +0300

    Remove references to nestjs-eve-auth in dev project and install local nestjs-clone-bay

--- dev/src/app.module.ts
+++ dev/src/app.module.ts
@@ -1,28 +1,24 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { EveAuthModule } from "nestjs-eve-auth";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from "./auth/auth.module";
-import { AuthService } from "./auth/auth.service";
 import { UserModule } from "./user/user.module";
 import { UserService } from "./user/user.service";
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URL),
-    EveAuthModule.forRootAsync({
-      useFactory: async (consumerService: AuthService) => ({
-        clientId: process.env.CLIENT_ID,
-        secretKey: process.env.SECRET_KEY,
-        callbackUrl: "http://localhost:3000/sso/callback",
-        afterLoginUrl: "http://localhost:3000",
-        service: consumerService,
-      }),
-      inject: [AuthService],
-      imports: [AuthModule],
-    }),
-    AuthModule,
+    // EveAuthModule.forRootAsync({
+    //   useFactory: async (consumerService: AuthService) => ({
+    //     clientId: process.env.CLIENT_ID,
+    //     secretKey: process.env.SECRET_KEY,
+    //     callbackUrl: "http://localhost:3000/sso/callback",
+    //     afterLoginUrl: "http://localhost:3000",
+    //     service: consumerService,
+    //   }),
+    //   inject: [AuthService],
+    //   imports: [AuthModule],
+    // }),
     UserModule,
   ],
   controllers: [AppController],
