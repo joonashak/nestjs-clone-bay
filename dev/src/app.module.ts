a08de5e3f15d6fcd974ad278619df8b51b86a3dd
joonashak <joonashak@proton.me>
2023-10-01 15:03:38 +0300

    Use nestjs-eve-auth from npm in dev

--- dev/src/app.module.ts
+++ dev/src/app.module.ts
@@ -1,24 +1,27 @@
+import { EveAuthModule } from "@joonashak/nestjs-eve-auth";
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { AuthService } from "./auth/auth.service";
 import { UserModule } from "./user/user.module";
 import { UserService } from "./user/user.service";
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URL),
-    // EveAuthModule.forRootAsync({
-    //   useFactory: async (consumerService: AuthService) => ({
-    //     clientId: process.env.CLIENT_ID,
-    //     secretKey: process.env.SECRET_KEY,
-    //     callbackUrl: "http://localhost:3000/sso/callback",
-    //     afterLoginUrl: "http://localhost:3000",
-    //     service: consumerService,
-    //   }),
-    //   inject: [AuthService],
-    //   imports: [AuthModule],
-    // }),
+    EveAuthModule.forRootAsync({
+      useFactory: async (authService: AuthService) => ({
+        clientId: process.env.CLIENT_ID,
+        secretKey: process.env.SECRET_KEY,
+        callbackUrl: "http://localhost:3000/sso/callback",
+        afterLoginUrl: "http://localhost:3000",
+        service: authService,
+      }),
+      inject: [AuthService],
+      imports: [AuthModule],
+    }),
     UserModule,
   ],
   controllers: [AppController],
