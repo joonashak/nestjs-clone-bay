f2e1c183260b893ad720b35a59e379fe0433e4e5
joonashak <joonashak@proton.me>
2023-12-06 10:40:34 +0200

    Clean up dev app

--- dev/src/app.module.ts
+++ dev/src/app.module.ts
@@ -4,25 +4,29 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { CloneBayModule } from "nestjs-clone-bay";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from "./auth/auth.module";
-import { AuthService } from "./auth/auth.service";
 import { UserModule } from "./user/user.module";
 import { UserService } from "./user/user.service";
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URL),
-    EveAuthModule.forRootAsync({
-      useFactory: async (authService: AuthService) => ({
-        clientId: process.env.CLIENT_ID,
-        secretKey: process.env.SECRET_KEY,
-        callbackUrl: "http://localhost:3000/sso/callback",
-        afterLoginUrl: "http://localhost:3000",
-        service: authService,
-      }),
-      inject: [AuthService],
-      imports: [AuthModule],
+    EveAuthModule.forRoot({
+      clientId: process.env.CLIENT_ID,
+      secretKey: process.env.SECRET_KEY,
+      callbackUrl: "http://localhost:3000/sso/callback",
+      afterLoginUrl: "http://localhost:3000",
     }),
+    // EveAuthModule.forRootAsync({
+    //   useFactory: async (authService: AuthService) => ({
+    //     clientId: process.env.CLIENT_ID,
+    //     secretKey: process.env.SECRET_KEY,
+    //     callbackUrl: "http://localhost:3000/sso/callback",
+    //     afterLoginUrl: "http://localhost:3000",
+    //     service: authService,
+    //   }),
+    //   inject: [AuthService],
+    //   imports: [AuthModule],
+    // }),
     UserModule,
     CloneBayModule,
   ],
