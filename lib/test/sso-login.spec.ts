d52080bd582e2cf88fb803d77e8d280d248cc4e5
joonashak <joonashak@proton.me>
2023-12-16 19:32:10 +0200

    Rewrite integration testing module to avoid having to intercept external HTTP requests

--- lib/test/sso-login.spec.ts
+++ lib/test/sso-login.spec.ts
@@ -1,25 +1,40 @@
-import { EveAuthModule } from "@joonashak/nestjs-eve-auth";
 import { INestApplication } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
+import { MongooseModule, getConnectionToken } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
 import session from "express-session";
 import request from "supertest";
-import { CloneBayModule } from "../src";
+import { AuthenticationAllowlistService } from "../src/authentication/authentication-allowlist.service";
+import { AuthenticationService } from "../src/authentication/authentication.service";
+import { CharacterService } from "../src/entities/character/character.service";
+import { User, UserSchema } from "../src/entities/user/user.model";
+import { UserService } from "../src/entities/user/user.service";
+import { SsoController } from "../src/sso.controller";
+import { MockEsiService } from "./mocks/esi-service.mock";
+import { provideMockDynamicConfigService } from "./mocks/mock-services";
+import { MockOAuthStrategy } from "./mocks/oauth.strategy.mock";
+import { MockSsoService } from "./mocks/sso-service.mock";
 
 describe("SSO login", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
+    // TODO: This is really messy, so need to make a proper testing module which mocks all the stuff that makes external requests.
+    // TODO: Add entity getters to real services or write testing utilities for db models directly?
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
-        CloneBayModule.forRoot({ testing: false }),
-        EveAuthModule.forRoot({
-          clientId: "asd",
-          secretKey: "dsa",
-          callbackUrl: "http://localhost:3000/sso/callback",
-          afterLoginUrl: "http://localhost:3000",
-        }),
         MongooseModule.forRoot(process.env.MONGO_URL),
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+      ],
+      controllers: [SsoController],
+      providers: [
+        AuthenticationAllowlistService,
+        AuthenticationService,
+        CharacterService,
+        MockEsiService,
+        MockOAuthStrategy,
+        MockSsoService,
+        provideMockDynamicConfigService(),
+        UserService,
       ],
     }).compile();
 
@@ -34,7 +49,8 @@ describe("SSO login", () => {
     await app.init();
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
+    await app.get(getConnectionToken()).dropDatabase();
     await app.close();
   });
 
@@ -44,8 +60,15 @@ describe("SSO login", () => {
       .expect(302)
       .redirects(0);
 
-    expect(res.headers.location).toMatch(
-      /^https:\/\/login.eveonline.com\/v2\/oauth\/authoriz/,
-    );
+    expect(res.headers.location).toMatch(/^https:\/\/example.com\/authorize/);
+  });
+
+  it("New user can login via SSO", async () => {
+    const res = await request(app.getHttpServer())
+      .get("/sso/callback?code=asd&state=asd")
+      .expect(302)
+      .redirects(0);
+
+    expect(res.headers.location).toEqual("/");
   });
 });
