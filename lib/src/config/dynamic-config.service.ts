980587c1618c3366b58f4ec43e568ce45bbc6b9a
joonashak <joonashak@proton.me>
2023-12-17 21:51:22 +0200

    Refactor dynamic config to use new cache service

--- lib/src/config/dynamic-config.service.ts
+++ lib/src/config/dynamic-config.service.ts
@@ -1,10 +1,9 @@
-import { CACHE_MANAGER } from "@nestjs/cache-manager";
-import { Inject, Injectable, Logger } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Cache } from "cache-manager";
 import { merge } from "lodash";
 import { Model } from "mongoose";
 import { PartialDeep } from "type-fest";
+import { CacheService } from "../cache/cache.service";
 import { DYNAMIC_CONFIG_CACHE_KEY } from "../constants";
 import { DynamicConfig, DynamicConfigDocument } from "./dynamic-config.model";
 
@@ -16,25 +15,17 @@ export class DynamicConfigService {
   constructor(
     @InjectModel(DynamicConfig.name)
     private dynamicConfigModel: Model<DynamicConfigDocument>,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private cacheService: CacheService,
   ) {}
 
   async get(): Promise<DynamicConfig> {
-    const cached = await this.cacheManager.get<DynamicConfig>(
-      DYNAMIC_CONFIG_CACHE_KEY,
+    return this.cacheService.wrap(DYNAMIC_CONFIG_CACHE_KEY, async () =>
+      this.dynamicConfigModel.findOne(),
     );
-
-    if (!cached) {
-      const dynamicConfig = await this.dynamicConfigModel.findOne();
-      this.cacheManager.set(DYNAMIC_CONFIG_CACHE_KEY, dynamicConfig);
-      return dynamicConfig;
-    }
-
-    return cached;
   }
 
   private async update(configUpdate: PartialDeep<DynamicConfig>) {
-    await this.cacheManager.del(DYNAMIC_CONFIG_CACHE_KEY);
+    await this.cacheService.del(DYNAMIC_CONFIG_CACHE_KEY);
     const current = await this.dynamicConfigModel.findOne();
     merge(current, configUpdate);
     return this.dynamicConfigModel.findOneAndUpdate({}, current, { new: true });
