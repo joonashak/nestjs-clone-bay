ed368aa936c46e78435feff18108d8c7f2ce3e2a
joonashak <joonashak@proton.me>
2023-12-25 18:07:05 +0200

    Make request utility compatible with GraphQL

--- lib/src/common/utils/get-request.util.ts
+++ lib/src/common/utils/get-request.util.ts
@@ -1,16 +1,15 @@
 import { BadRequestException, ExecutionContext } from "@nestjs/common";
-// TODO: Do we want to depend on this or think of something else?
-// import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
+import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
 
 const getRequest = (context: ExecutionContext) => {
   if (context.getType() === "http") {
     return context.switchToHttp().getRequest();
   }
 
-  // if (context.getType<GqlContextType>() === "graphql") {
-  //   const gqlContext = GqlExecutionContext.create(context);
-  //   return gqlContext.getContext().req;
-  // }
+  if (context.getType<GqlContextType>() === "graphql") {
+    const gqlContext = GqlExecutionContext.create(context);
+    return gqlContext.getContext().req;
+  }
 
   throw new BadRequestException(
     "Server only accepts HTTP and GraphQL requests.",
