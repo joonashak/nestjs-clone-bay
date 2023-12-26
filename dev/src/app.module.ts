e32e64a0773b3dc594f5bf6e53166f18098bfd2e
joonashak <joonashak@proton.me>
2023-12-26 10:28:11 +0200

    Switch to Apollo Sandbox for easier authentication in dev env

--- dev/src/app.module.ts
+++ dev/src/app.module.ts
@@ -1,3 +1,4 @@
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 import { EveAuthModule } from "@joonashak/nestjs-eve-auth";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
@@ -11,13 +12,22 @@ import {
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
+/*
+ * WARNING!
+ * This configuration is for local development use only. Using this kind of
+ * configuration in production would be extremely dangerous.
+ */
+
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URL),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
-      playground: true,
+      playground: false,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault({ includeCookies: true }),
+      ],
     }),
     EveAuthModule.forRoot({
       clientId: process.env.CLIENT_ID,
@@ -25,17 +35,6 @@ import { AppService } from "./app.service";
       callbackUrl: "http://localhost:3000/sso/callback",
       afterLoginUrl: "http://localhost:3000",
     }),
-    // EveAuthModule.forRootAsync({
-    //   useFactory: async (authService: AuthService) => ({
-    //     clientId: process.env.CLIENT_ID,
-    //     secretKey: process.env.SECRET_KEY,
-    //     callbackUrl: "http://localhost:3000/sso/callback",
-    //     afterLoginUrl: "http://localhost:3000",
-    //     service: authService,
-    //   }),
-    //   inject: [AuthService],
-    //   imports: [AuthModule],
-    // }),
     CloneBayModule.forRoot({ testing: true }),
     CloneBaySsoModule,
     CloneBayResolversModule,
--- dev/src/main.ts
+++ dev/src/main.ts
@@ -5,6 +5,12 @@ import session from "express-session";
 import mongoose from "mongoose";
 import { AppModule } from "./app.module";
 
+/*
+ * WARNING!
+ * This configuration is for local development use only. Using this kind of
+ * configuration in production would be extremely dangerous.
+ */
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
