ceb85b571d125d7f7dc84c4b23af546ec966a85a
joonashak <joonashak@proton.me>
2024-11-23 10:25:12 +0200

    Add mocking module route to remove users and sessions

--- lib/src/mocking/mocking.service.ts
+++ lib/src/mocking/mocking.service.ts
@@ -1,11 +1,17 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectConnection } from "@nestjs/mongoose";
+import { Connection } from "mongoose";
 import { setUserId } from "../common/utils/session.util";
 import { User } from "../entities/user/user.model";
 import { UserService } from "../entities/user/user.service";
 
 @Injectable()
 export class MockingService {
-  constructor(private userService: UserService) {}
+  constructor(
+    @InjectConnection() private dbConnection: Connection,
+
+    private userService: UserService,
+  ) {}
 
   async createUser(user: Omit<User, "id">) {
     return this.userService.create(user);
@@ -20,5 +26,15 @@ export class MockingService {
 
     setUserId(session, user.id);
     return user;
+  }
+
+  /**
+   * Removes all users and sessions.
+   *
+   * Note that dynamic config is left untouched.
+   */
+  async reset() {
+    await this.dbConnection.dropCollection("users");
+    await this.dbConnection.dropCollection("sessions");
   }
 }
