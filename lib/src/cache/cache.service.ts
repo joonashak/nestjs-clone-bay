bbcf3fdb70580f54d89859c65194473c27988ce8
joonashak <joonashak@proton.me>
2025-03-02 17:26:12 +0200

    Update to Nest.js v11

--- lib/src/cache/cache.service.ts
+++ lib/src/cache/cache.service.ts
@@ -1,16 +1,16 @@
 import { CACHE_MANAGER } from "@nestjs/cache-manager";
 import { Inject, Injectable } from "@nestjs/common";
-import { Cache, WrapTTL } from "cache-manager";
+import { Cache } from "cache-manager";
 
 @Injectable()
 export class CacheService {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-  wrap<T>(key: string, fn: () => Promise<T>, ttl?: WrapTTL<T>): Promise<T> {
+  wrap<T>(key: string, fn: () => Promise<T>, ttl?: number): Promise<T> {
     return this.cacheManager.wrap(key, fn, ttl);
   }
 
-  del(key: string): Promise<void> {
+  del(key: string): Promise<boolean> {
     return this.cacheManager.del(key);
   }
 }
