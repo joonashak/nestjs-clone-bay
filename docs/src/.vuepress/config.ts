51306dd56b134d2258de4ad1dcf579706c0b613b
joonashak <joonashak@proton.me>
2025-03-04 17:58:10 +0200

    Upgrade VuePress to latest RC

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -1,8 +1,11 @@
-import { defaultTheme, defineUserConfig } from "vuepress";
-import { copyCodePlugin } from "vuepress-plugin-copy-code2";
+import { viteBundler } from "@vuepress/bundler-vite";
+import { copyCodePlugin } from "@vuepress/plugin-copy-code";
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress";
 import sidebar from "./sidebar";
 
 export default defineUserConfig({
+  bundler: viteBundler(),
   lang: "en-US",
   title: "nestjs-clone-bay docs",
   description: "Documentation for @joonashak/nestjs-clone-bay library.",
