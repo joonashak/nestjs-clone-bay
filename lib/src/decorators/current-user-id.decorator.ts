50d48d4103e4ec85b774a5cd265a965e030c879e
joonashak <joonashak@proton.me>
2025-03-11 18:40:11 +0200

    Update docs

--- lib/src/decorators/current-user-id.decorator.ts
+++ lib/src/decorators/current-user-id.decorator.ts
@@ -23,4 +23,33 @@ export const decoratorFunction = (
   }
 };
 
+/**
+ * Inject authenticated user's ID into method argument.
+ *
+ * This is Clone Bay's internal UUID, not EVE/ESI ID.
+ *
+ * Accepts an optional `options` configuration object to allow a missing user.
+ * If `options.nullable` is set to `true`, this decorator will return `null` for
+ * a missing user. Otherwise throws HTTP 401.
+ *
+ * ### Examples:
+ *
+ * ```ts
+ * @Get("hello")
+ * async hello(@CurrentUserId() userId: string) {
+ *   // Will throw if user is not found.
+ * }
+ * ```
+ *
+ * ```ts
+ * @Get("hello")
+ * async hello(@CurrentUserId({ nullable: true }) userId: string | undefined) {
+ *   // `userId` is `undefined` if not authenticated or not found.
+ *   // No error is thrown.
+ * }
+ * ```
+ *
+ * @returns `string | null`
+ * @group Decorators
+ */
 export const CurrentUserId = createParamDecorator<CurrentUserIdDecoratorOptions>(decoratorFunction);
